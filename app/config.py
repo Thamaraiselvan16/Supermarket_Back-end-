a9e118f6c8634a0ae6b33b2c2189801506a983e3
Thamaraiselvan16 <143943463+Thamaraiselvan16@users.noreply.github.com>
2024-06-14 12:36:50 +0530

    13-06-2024

--- app/config.py
+++ app/config.py
@@ -4,12 +4,12 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:thamarai5118@localhost/supermarket_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://user:password@localhost/database_name')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     
     EMAIL_SERVER = 'smtp.gmail.com'
     EMAIL_PORT = 587
-    EMAIL = os.getenv('EMAIL', 'dhill7228@gmail.com')
-    EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD', 'pvgx jamx fvgt orpj')
-    MANAGER_EMAIL = os.getenv('MANAGER_EMAIL', 'tthamaraiselvan2002@gmail.com')
+    EMAIL = os.getenv('EMAIL', 'your_mail@gmail.com')
+    EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD', 'llll bbbb cccc nnnn')
+    MANAGER_EMAIL = os.getenv('MANAGER_EMAIL', 'manager_mail@gmail.com')
